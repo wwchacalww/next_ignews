8aa1cd3ad5249b3a67156b9f258a717877db9818
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-17 14:26:53 -0300

    🎨 Layout home created

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
+import styles from './home.module.scss';
 
 const Home: NextPage = () => {
   return (
     <>
-      <Head><title>Inicio | ig.news</title></Head>
-      <h1>
-        Hello World
-      </h1>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+          <p>
+            Get access to all publications <br />
+            <span>for $9.90 month</span>
+          </p>
+          <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
