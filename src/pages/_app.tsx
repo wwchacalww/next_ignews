ae85501e9997e53a826b93c16fad7572a6acfe73
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-18 11:54:37 -0300

    🔑 NextAuth implemented

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import type { AppProps } from 'next/app';
 import { Header } from '../components/Header';
+import { SessionProvider as NextAuthProviders} from "next-auth/react"
+
+
 import '../styles/global.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <NextAuthProviders session={pageProps}>
       <Header />
       <Component {...pageProps} />
-    </>
+    </ NextAuthProviders>
   )
 }
 
