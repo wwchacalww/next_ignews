0a10f72a2f19353f073c3bba0a78f6a530302e69
Vicente Cartaxo <juniorcartaxo@gmail.com>
2022-06-19 18:19:05 -0300

    ⚙ Set Api FaunaDB

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -6,7 +6,6 @@ import styles from './styles.module.scss';
 
 export function SignInButton() {
   const session = useSession();
-  console.log(session);
   return session.data ? 
   (
     <button 
--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from "faunadb";
+import { faunadb } from "../../../services/faunadb";
+
 export default NextAuth({
   providers: [
     GithubProvider({
@@ -12,4 +15,24 @@ export default NextAuth({
       },
     }),
   ],
+  secret: process.env.SINGING_KEY,
+  callbacks: {
+    async signIn({ user }) {
+      const email = user.email || "teste";
+      try {
+        await faunadb.query(
+          q.If(
+            q.Not(
+              q.Exists(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(email)))
+          )
+        );
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
